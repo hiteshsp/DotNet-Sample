d1c227cfd10f6b2a8a271b1b995c182aaf7a6709
HA <h@a.com>
2017-11-06 11:25:36 +0000

    Added Tests

---
+++ TestAppTests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestApp.Controllers.Tests
+{
+    [TestClass()]
+    public class AccountControllerTests
+    {
+        [TestMethod()]
+        public void GetUserInfoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetManageInfoTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
